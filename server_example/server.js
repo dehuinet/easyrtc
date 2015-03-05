a16c3177055e9c9fd3b54a5ffb6b02bf3c1f16c6
skar9363 <skar9363@gmail.com>
2015-03-05 11:28:18 +0800

    set easyrtc turn servers option

--- server_example/server.js
+++ server_example/server.js
@@ -14,5 +14,28 @@ var webServer = http.createServer(httpApp).listen(8080);
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
 
+easyrtc.setOption('appIceServers', [
+	{url: "stun:stun.l.google.com:19302"},
+    {url: "stun:stun.sipgate.net"},
+    {url: "stun:217.10.68.152"},
+    {url: "stun:stun.sipgate.net:10000"},
+    {url: "stun:217.10.68.152:10000"},
+    /*{
+		'url': 'turn:numb.viagenie.ca:3478',
+		'credential': '86052623',
+		'username': 'skar9363@163.com'
+	}*/
+	{
+	    "url":"turn:turn.anyfirewall.com:3478?transport=udp",
+	    "username":"1421510621:KellyApp",
+	    "credential":"a2w1Z2kwZ2Y"
+	  },
+	  {
+	    "url":"turn:turn.anyfirewall.com:443?transport=tcp",
+	    "username":"1421510621:KellyApp",
+	    "credential":"a2w1Z2kwZ2Y"
+	  }
+]);
+
 // Start EasyRTC server
 var rtc = easyrtc.listen(httpApp, socketServer);
