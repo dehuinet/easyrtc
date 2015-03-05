421c9f170515a1cf937982a536733c5cabe93c36
skar9363 <skar9363@gmail.com>
2015-03-05 15:47:49 +0800

    异步请求stun/turn服务器

--- server_example/server.js
+++ server_example/server.js
@@ -3,6 +3,7 @@ var http    = require("http");              // http server core module
 var express = require("express");           // web framework external module
 var io      = require("socket.io");         // web socket external module
 var easyrtc = require("easyrtc");           // EasyRTC external module
+var request = require("request");
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
@@ -14,28 +15,32 @@ var webServer = http.createServer(httpApp).listen(8066);
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
 
-easyrtc.setOption('appIceServers', [
-	{url: "stun:stun.l.google.com:19302"},
-    {url: "stun:stun.sipgate.net"},
-    {url: "stun:217.10.68.152"},
-    {url: "stun:stun.sipgate.net:10000"},
-    {url: "stun:217.10.68.152:10000"},
-    /*{
-		'url': 'turn:numb.viagenie.ca:3478',
-		'credential': '86052623',
-		'username': 'skar9363@163.com'
-	}*/
-	{
-	    "url":"turn:turn.anyfirewall.com:3478?transport=udp",
-	    "username":"1421510621:KellyApp",
-	    "credential":"a2w1Z2kwZ2Y"
-	  },
-	  {
-	    "url":"turn:turn.anyfirewall.com:443?transport=tcp",
-	    "username":"1421510621:KellyApp",
-	    "credential":"a2w1Z2kwZ2Y"
-	  }
-]);
+easyrtc.on("getIceConfig", function(connectionObj, callback) {
+    // This object will take in an array of XirSys STUN and TURN servers
+    var iceConfig = [];
+
+    request.post('https://api.xirsys.com/getIceServers', {
+        form: {
+            ident: "bingoli",
+            secret: "890fec8e-19ce-44f3-9277-17dabb222f50",
+            domain: "rtcdemo",
+            application: "default",
+            room: "rtcdemo",
+            secure: 1
+        },
+        json: true
+    },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // body.d.iceServers is where the array of ICE servers lives
+            iceConfig = body.d.iceServers;
+            console.log(iceConfig);
+            callback(null, iceConfig);
+        } else {
+       		console.info('请求turn数据失败', body);
+        }
+    });
+});
 
 // Start EasyRTC server
 var rtc = easyrtc.listen(httpApp, socketServer);
